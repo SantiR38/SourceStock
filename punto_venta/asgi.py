28d8fd7c87df5a8b3356dcfdc3abe105d330d25e
SantiR38 <santirodriguez38@gmail.com>
2020-08-19 10:09:48 -0300

    Obtencion automática de direccion ip.

--- punto_venta/asgi.py
+++ punto_venta/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'punto_venta.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'punto_venta.settings.deploy')
 
 application = get_asgi_application()
