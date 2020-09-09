8810efc918ef9ca1b074a4d10f153af34a194f56
SantiR38 <santirodriguez38@gmail.com>
2020-09-08 22:42:01 -0300

    Vista venta_catalogo ahora muestra el detalle de la venta en curso y permite cancelar unidades.

--- venta_catalogo/views.py
+++ venta_catalogo/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse, FileResponse
 from django.template import Template, Context, loader
 from erp.models import Article
 from venta_catalogo.forms import FormFiltrarArticulos
-from erp.functions import inventario, stock_total
+from erp.functions import inventario, stock_total, venta_activa
 from .functions.search_engines import search_articles
 
 
@@ -11,6 +11,8 @@ def venta_por_catalogo(request):
     template = loader.get_template('venta_catalogo/venta.html')
     miFormulario = FormFiltrarArticulos()
     ctx = {
+        "articulo_a_vender": venta_activa()[0],
+        "totales": venta_activa()[1],
         "datos_generales": stock_total(),
         "articulos": inventario(Article).order_by('descripcion'),
         "carrito": "",
@@ -29,5 +31,8 @@ def venta_por_catalogo(request):
     return HttpResponse(template.render(ctx, request))
 
 def aniadir_al_carrito(request, codigo_param):
+
+    new_article = Article.objects.get(codigo=codigo_param)
+
     
     return redirect('venta_por_catalogo')
