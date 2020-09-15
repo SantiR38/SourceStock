ceda9e5eb66e5fecf35bd978f0a93136468b05bb
SantiR38 <santirodriguez38@gmail.com>
2020-09-15 16:54:39 -0300

    Implementacion de buenas practicas en sintaxis.

--- punto_venta/urls.py
+++ punto_venta/urls.py
@@ -13,12 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from datetime import date
+#from datetime import date
 from django.contrib import admin
 from django.urls import include, path
-from django.contrib.auth.views import LoginView
-from django.conf.urls import handler404
-from erp.views import error_404
+#from django.conf.urls import handler404
+#from erp.views import error_404
 
 urlpatterns = [
         path('admin/', admin.site.urls),
@@ -41,4 +40,4 @@ else:
         path('accounts/', include('django.contrib.auth.urls')),
     ]
 '''
-handler404 = error_404
+#handler404 = error_404
