bbe6adafcda5cced39fa698f2b2771f87e888b87
Santi Rodriguez <santirodriguez38@gmail.com>
2021-07-14 05:04:45 +0200

    Connection errors with dolar api handled.

--- api/models.py
+++ api/models.py
@@ -4,27 +4,42 @@ from django.db import models
 
 from .services import get_dolar_value
 # Create your models here.
-
-DOLAR_VALUES = get_dolar_value()
-DOLAR_COMPRA = Decimal(DOLAR_VALUES[0].replace(',','.'))
-DOLAR_VENTA = Decimal(DOLAR_VALUES[1].replace(',','.'))
+REQ_STATUS = 'pending'
+try:
+    DOLAR_VALUES = get_dolar_value()
+except TypeError:
+    DOLAR_COMPRA = None
+    DOLAR_VENTA = None
+    REQ_STATUS = 'error'
+else:
+    DOLAR_COMPRA = Decimal(DOLAR_VALUES[0].replace(',','.'))
+    DOLAR_VENTA = Decimal(DOLAR_VALUES[1].replace(',','.'))
+    REQ_STATUS = 'ok'
 
 
 class PrecioDolar(models.Model):
-    oficial_compra = models.DecimalField(max_digits=10, decimal_places=2, null=True, default=DOLAR_COMPRA)
-    oficial_venta = models.DecimalField(max_digits=10, decimal_places=2, null=True, default=DOLAR_VENTA)
+    oficial_compra = models.DecimalField(max_digits=10, decimal_places=2,
+        null=True, default=DOLAR_COMPRA)
+    oficial_venta = models.DecimalField(max_digits=10, decimal_places=2,
+        null=True, default=DOLAR_VENTA)
 
     @classmethod
     def actualizar_registro(cls):
-        registry = cls.objects.filter(id=1)
-        if registry.exists():
-            obj = cls.objects.get(id=1)
-            obj.oficial_compra = DOLAR_COMPRA
-            obj.oficial_venta = DOLAR_VENTA
-            obj.save()
-        else:
-            cls.objects.create()
+        if REQ_STATUS == 'ok':
+            registry = cls.objects.filter(id=1)
+            if registry.exists():
+                obj = cls.objects.get(id=1)
+                obj.oficial_compra = DOLAR_COMPRA
+                obj.oficial_venta = DOLAR_VENTA
+                obj.save()
+            else:
+                cls.objects.create(oficial_venta=DOLAR_VENTA,
+                    oficial_compra=DOLAR_COMPRA)
     
     @classmethod
     def cotizacion_venta(cls):
-        return cls.objects.get(id=1).oficial_venta
+        try:
+            ret = cls.objects.get(id=1).oficial_venta
+        except cls.DoesNotExist:
+            ret = ''
+        return ret
--- punto_venta/context_processors.py
+++ punto_venta/context_processors.py
@@ -1,12 +1,15 @@
 """Context processor."""
 
 # SourceStock - Api
+from api.models import PrecioDolar
 from api.services import get_dolar_value
-
 
 # ------ SERVICES ------ #
 
 def sample_context_processor(request):
     """Pasar el precio del dolar por contexto para el navbar."""
-
-    return {'dolar':get_dolar_value()[1]}
+    try:
+        dolar_value = get_dolar_value()[1]
+    except TypeError:
+        dolar_value = PrecioDolar.cotizacion_venta()
+    return {'dolar':dolar_value}
