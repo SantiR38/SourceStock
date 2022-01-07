1d5fff1a358b8925323b4ed9e529a96a5f0a52f0
SantiR <santirodriguez38@gmail.com>
2022-01-07 09:08:17 -0300

    Messages app refactored.

--- messages/urls.py
+++ messages/urls.py
@@ -1,14 +1,9 @@
 """Messages urls."""
-
-#Django
 from django.urls import path, include
 
-# SourceStock - messages
 from .views import TimesUpView
 
 
 urlpatterns = [
-
     path('times_up', TimesUpView.as_view(), name='times_up'),
-
 ]
