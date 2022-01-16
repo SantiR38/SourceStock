5f84e52e4acaf67fb016c75d7f2b45eac5737795
SantiR <santirodriguez38@gmail.com>
2022-01-15 23:39:04 -0300

    Changed settings path.

--- punto_venta/wsgi.py
+++ punto_venta/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'punto_venta.settings.production')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'punto_venta.settings.settings')
 
 application = get_wsgi_application()
